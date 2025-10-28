573e3bf03b78846fc707358ffc9dac8dac65de27
mukulsingh24 <rmks1004@gmail.com>
2025-10-28 15:33:44 +0530

    ExpenseFrom completed basic

--- Frontend/src/components/ExpenseFrom.jsx
+++ Frontend/src/components/ExpenseFrom.jsx
@@ -5,13 +5,25 @@ function Form() {
     const [expense,setExpense] = useState("");
     const [amount,setAmount] = useState(0);
     const [type,setType] = useState("Personal")
+
+    const updateExpense = (e) =>{
+        setExpense(e.target.value)
+    }
+
+    const updateAmount = (e) =>{
+        setAmount(e.target.value)
+    }
+
+    const updateType = (e) =>{
+        setType(e.target.value)
+    }
   return (
     <div>
       <h1>Expense Tracker Form</h1>
-      <input type="text" placeholder="Enter Your Expense" />
-      <input type="text" placeholder="Enter the Amount"/>
-      <label for="cars">Type</label>
-      <select name="Expense Type">
+      <input type="text" value={expense} onChange={updateExpense} placeholder="Enter Your Expense" />
+      <input type="text" value={amount} onChange={updateAmount} placeholder="Enter the Amount"/>
+      <label htmlfor="cars">Type</label>
+      <select name="Expense Type" value={type} onChange={updateType}>
         <option value="Personal">Personal</option>
         <option value="Stocks">Stocks</option>
         <option value="Financial">Financial</option>
